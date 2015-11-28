a88f284e2d0bb1c5d623e8c5ed9ee3ec590e1dcc
parkerliu22 <parkerliu22@126.com>
2015-11-28 20:52:33 +0800

    First Commit

--- GSpent/BookListViewController.swift
+++ GSpent/BookListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  BookListViewController.swift
 //  GSpent
 //
 //  Created by Jiahe Liu on 28/11/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class BookListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- GSpent/PersonalTallyViewController.swift
+++ GSpent/PersonalTallyViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  PersonalTallyViewController.swift
 //  GSpent
 //
 //  Created by Jiahe Liu on 28/11/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class PersonalTallyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
