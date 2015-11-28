09ced680509e9eef943be002d2d95cb2e4c43e3f
parkerliu22 <parkerliu22@126.com>
2015-11-28 20:19:37 +0800

    Initial Commit

---
+++ GSpent/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GSpent
+//
+//  Created by Jiahe Liu on 28/11/15.
+//  Copyright © 2015年 LIU Jiahe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
