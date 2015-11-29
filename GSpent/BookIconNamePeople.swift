033acddcf31da56a720b279b01f901d14a9f6b8b
curme <wiskyzhan@gmail.com>
2015-11-29 17:13:34 +0800

    add new ui

---
+++ GSpent/BookIconNamePeople.swift
@@ -0,0 +1,28 @@
+//
+//  BookIconNamePeople.swift
+//  GSpent
+//
+//  Created by HUI ZHAN on 11/29/15.
+//  Copyright © 2015 LIU Jiahe. All rights reserved.
+//
+
+import UIKit
+
+class BookIconNamePeople: UITableViewCell {
+
+    @IBOutlet weak var BookIcon: UIImageView!
+    @IBOutlet weak var BookName: UILabel!
+    @IBOutlet weak var BookPeople: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
