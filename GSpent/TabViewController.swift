57b8220927db14b4379ed1f4cae83af623f35c50
parker22 <cn.parker22@gmail.com>
2015-11-29 14:45:58 +0800

    Added Center Raised Tab

---
+++ GSpent/TabViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TabViewController.swift
+//  GSpent
+//
+//  Created by Jiahe Liu on 29/11/15.
+//  Copyright © 2015年 LIU Jiahe. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: RaisedTabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Insert empty tab item at center index. In this case we have 5 tabs.
+        self.insertEmptyTabItem("", atIndex: 1)
+        
+        // Raise the center button with image
+        let img = UIImage(named: "icon_camera")
+        self.addRaisedButton(img, highlightImage: nil)
+    }
+    
+    // Handler for raised button
+    override func onRaisedButton(sender: UIButton!) {
+        super.onRaisedButton(sender)
+        
+        print("Raised button tapped")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
