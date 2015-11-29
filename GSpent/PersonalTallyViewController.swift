755d40589235ffc731f023496c71e72ca886563d
curme <curmium@gamil.com>
2015-11-29 14:26:30 +0800

    update

--- GSpent/PersonalTallyViewController.swift
+++ GSpent/PersonalTallyViewController.swift
@@ -18,6 +18,7 @@ class PersonalTallyViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // here
     }
 
 
