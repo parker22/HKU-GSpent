17fdbbffab64b0594637c7551dca479d7bda61d9
curme <wiskyzhan@gmail.com>
2015-12-01 12:58:07 +0800

    simple modify tally layout

---
+++ GSpent/TallyBookSelect.swift
@@ -0,0 +1,13 @@
+//
+//  TallyBookSelect.swift
+//  GSpent
+//
+//  Created by HUI ZHAN on 12/1/15.
+//  Copyright © 2015 LIU Jiahe. All rights reserved.
+//
+
+import Cocoa
+
+class TallyBookSelect: UITableViewController {
+
+}
