43a7336504a73394e43360c4029d9f82db0195c6
parker22 <cn.parker22@gmail.com>
2015-12-02 23:36:29 +0800

    什么都没干

---
+++ GSpent/BookDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BookDetailViewController.swift
+//  GSpent
+//
+//  Created by Jiahe Liu on 2/12/15.
+//  Copyright © 2015年 LIU Jiahe. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController,UICollectionViewDelegate {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
