bb80baed5722d3346db6f8a5082866c62de1cf2f
laurenceyoung <laurenceyoung@hku.hk>
2015-12-03 15:45:14 +0800

    test by laurence

--- GSpent/TabViewController.swift
+++ GSpent/TabViewController.swift
@@ -20,25 +20,25 @@ class TabViewController: RaisedTabBarController {
         let img = UIImage(named: "tabbar_item_new_spent")
         self.addRaisedButton(img, highlightImage: nil)
 
-        addBook = UIButton(frame: CGRect(x: 100, y: 630, width: 50, height: 50))
-        addBook.setImage(UIImage(named: "bookIconSample00"), forState: UIControlState.Normal)
-        addBook.titleLabel?.font = UIFont.boldSystemFontOfSize(30)
-        addBook.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
-        //addBook.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        addBook.setTitle("add a new book", forState: UIControlState.Normal)
-        addBook.addTarget(self, action: Selector("addBook:"), forControlEvents: UIControlEvents.TouchUpInside)
-        addBook.hidden = true
-        self.view.addSubview(addBook)
-        
-        addSpent = UIButton(frame: CGRect(x: 264, y: 630, width: 50, height: 50))
-        addSpent.setImage(UIImage(named: "bookIconSample00"), forState: UIControlState.Normal)
-        addSpent.titleLabel?.font = UIFont.boldSystemFontOfSize(30)
-        addSpent.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
-        //addSpent.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        addSpent.setTitle("add a new spent", forState: UIControlState.Normal)
-        addSpent.addTarget(self, action: Selector("addSpent:"), forControlEvents: UIControlEvents.TouchUpInside)
-        addSpent.hidden = true
-        self.view.addSubview(addSpent)
+//        addBook = UIButton(frame: CGRect(x: 100, y: 630, width: 50, height: 50))
+//        addBook.setImage(UIImage(named: "bookIconSample00"), forState: UIControlState.Normal)
+//        addBook.titleLabel?.font = UIFont.boldSystemFontOfSize(30)
+//        addBook.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+//        //addBook.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//        addBook.setTitle("add a new book", forState: UIControlState.Normal)
+//        addBook.addTarget(self, action: Selector("addBook:"), forControlEvents: UIControlEvents.TouchUpInside)
+//        addBook.hidden = true
+//        self.view.addSubview(addBook)
+//        
+//        addSpent = UIButton(frame: CGRect(x: 264, y: 630, width: 50, height: 50))
+//        addSpent.setImage(UIImage(named: "bookIconSample00"), forState: UIControlState.Normal)
+//        addSpent.titleLabel?.font = UIFont.boldSystemFontOfSize(30)
+//        addSpent.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+//        //addSpent.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//        addSpent.setTitle("add a new spent", forState: UIControlState.Normal)
+//        addSpent.addTarget(self, action: Selector("addSpent:"), forControlEvents: UIControlEvents.TouchUpInside)
+//        addSpent.hidden = true
+//        self.view.addSubview(addSpent)
 
     }
 
@@ -52,9 +52,10 @@ class TabViewController: RaisedTabBarController {
     }
     
     override func onRaisedButton(sender: UIButton!) {
-        let ifHide = self.addBook.hidden&&self.addSpent.hidden
-        if (ifHide){ extraButtonSetShow() }
-        else       { extraButtonSetHide() }
+//        let ifHide = self.addBook.hidden&&self.addSpent.hidden
+//        if (ifHide){ extraButtonSetShow() }
+//        else       { extraButtonSetHide() }
+        performSegueWithIdentifier("showNewSpentSegue", sender: self)
         print("Raised button tapped")
     }
 
