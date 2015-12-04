6e3a9606cfed04b10ba9edfd7be0c3bf7904aeb7
laurenceyoung <laurenceyoung@hku.hk>
2015-12-04 15:34:50 +0800

    settlement partial implementation

--- GSpent/SettlementViewController.swift
+++ GSpent/SettlementViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class SettlementViewController: UIViewController {
 
+    @IBAction func generateSettlement(sender: AnyObject) {
+        var person1s = [String]()
+        var person2s = [String]()
+        
+        let str = "{\"books\": [\"person1\", \"person2\", \"person3\"]}"
+        let nsData = str.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+        //TODO:obtain server NSData here...
+        
+        do {
+            let json = try NSJSONSerialization.JSONObjectWithData(nsData,options: .AllowFragments)
+            
+            if let books = json["books"] as? [[String: AnyObject]] {
+                for book in books {
+                    if let person = book["person1"] as? String {
+                        person1s.append(person)
+                    }
+                    if let person = book["person2"] as? String {
+                        person2s.append(person)
+                    }
+                }
+            }
+            
+        } catch {
+            print("error reading JSON data: \(error)")
+        }
+        
+        print(person1s) // ["Bloxus test", "Manila Test"]
+    }
+    
+    @IBAction func share(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
