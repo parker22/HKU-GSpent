4c324c139a8b54d41e5b08a6b64243d87c44cfbd
curme <wiskyzhan@gmail.com>
2015-12-08 10:59:41 +0800

    remove leanCloud

--- Pods/Headers/Public/AVOSCloudDynamic/AVOSCloud/AVPush.h
+++
@@ -1,1 +0,0 @@
-../../../../AVOSCloudDynamic/iOS/release-v3.1.6.7/Dynamic/AVOSCloud.framework/Headers/AVPush.h
--- Pods/Headers/Public/AVOSCloudIMDynamic/AVOSCloudIM/AVIMFileMessage.h
+++
@@ -1,1 +0,0 @@
-../../../../AVOSCloudIMDynamic/iOS/release-v3.1.6.7/Dynamic/AVOSCloudIM.framework/Headers/AVIMFileMessage.h
