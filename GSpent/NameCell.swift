f1bc9bbb63547b7627b1474d9858a82d1caa77f9
laurenceyoung <laurenceyoung@hku.hk>
2015-12-10 19:28:45 +0800

    cells

---
+++ GSpent/NameCell.swift
@@ -0,0 +1,15 @@
+//
+//  NameCell.swift
+//  GSpent
+//
+//  Created by Laurence on 10/12/2015.
+//  Copyright © 2015 LIU Jiahe. All rights reserved.
+//
+
+import UIKit
+
+class NameCell: UITableViewCell {
+
+    @IBOutlet weak var NameLabel: UILabel!
+    
+}
---
+++ GSpent/RecordCell.swift
@@ -0,0 +1,14 @@
+//
+//  RecordCell.swift
+//  GSpent
+//
+//  Created by Laurence on 10/12/2015.
+//  Copyright © 2015 LIU Jiahe. All rights reserved.
+//
+
+import UIKit
+
+class RecordCell: UITableViewCell {
+
+    @IBOutlet weak var recordLabel: UILabel!
+}
