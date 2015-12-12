a0ccfb6051227e816c62d37e0b4fe1622d683ef2
laurenceyoung <laurenceyoung@hku.hk>
2015-12-13 01:47:14 +0800

    12223

---
+++ GSpent/SelectionSingleMemberTVCell.swift
@@ -0,0 +1,13 @@
+//
+//  SelectionSingleMemberTVCell.swift
+//  GSpent
+//
+//  Created by Laurence on 13/12/2015.
+//  Copyright © 2015 LIU Jiahe. All rights reserved.
+//
+
+import UIKit
+
+class SelectionSingleMemberTVCell: UITableViewCell {
+
+}
