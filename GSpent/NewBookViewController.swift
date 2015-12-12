0fe3ad67f000b3df64c55a317ba373b4665cfefd
laurenceyoung <laurenceyoung@hku.hk>
2015-12-12 19:50:39 +0800

    new book

--- GSpent/NewBookViewController.swift
+++ GSpent/NewBookViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Parse
 
 class NewBookViewController: UIViewController,sendBookMemberBack {
+    
+    var members:[Int]=[]
+    var membersID = Dictionary<String, String>()
     
     @IBOutlet weak var selectBookMemberBtn: UIButton!
 
@@ -26,10 +30,36 @@ class NewBookViewController: UIViewController,sendBookMemberBack {
     
     @IBAction func submitNewBook(sender: AnyObject) {
         //do some thing
+        var newBook = PFObject(className:"Book")
+        newBook["b_average"] = 1337
+        newBook["u_id"] = 25
+        //newBook["ACL"] = "Public Read + Write"//ACL
+        newBook["b_icon"] = UIImage()//File
+        if members.count == 0{
+            //popup a window : please select member
+        }
+        newBook["b_participant"] = [PFObject]()//Array {"objectId":"DfOI3qgAh9"}
+        newBook["b_id"]=9
+        
+        newBook.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                // The object has been saved.
+            } else {
+                // There was a problem, check error.description
+            }
+        }
+        
     }
+    
+    
     @IBAction func cancelAddBook(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil);
 
+    }
+    
+    
+    @IBAction func chooseMembers(sender: AnyObject) {
     }
     
     
